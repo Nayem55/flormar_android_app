d60e861398d3e7c3febb12e541a30acb4e1dbffd
unknown <md.monirjhossain@gmail.com>
2023-06-11 12:32:54 +0600

    Created Category setion

---
+++ src/Components/Categories.js
@@ -0,0 +1,61 @@
+import { View, Image, Text } from 'react-native'
+import React from 'react'
+
+const Categories = () => {
+  return (
+    <View style={{flex:1, flexDirection:'row', padding:10, paddingTop:20}}>
+        <View style={styles.category}>
+        <Image style = {styles.image}  source={require('../Images/categories/cat1.png')} />
+            <Text>
+                Face
+            </Text>
+        </View>
+        <View style={styles.category}>
+        <Image style = {styles.image} source={require('../Images/categories/cat2.png')} />
+            <Text>
+                Eyes    
+            </Text>
+        </View>
+        <View style={styles.category}>
+        <Image style = {styles.image} source={require('../Images/categories/cat3.png')} />
+            <Text>
+                Lips
+            </Text>
+        </View>
+        <View style={styles.category}>
+        <Image style = {styles.image} source={require('../Images/categories/cat4.png')} />
+            <Text>
+                Nails
+            </Text>
+        </View>
+        <View style={styles.category}>
+        <Image style = {styles.image} source={require('../Images/categories/cat5.png')} />
+            <Text>
+                Accessories
+            </Text>
+        </View>
+    </View>
+  )
+}
+
+export default Categories;
+
+const styles = {
+    category : {
+        flex:1,
+        justifyContent:"center",
+        alignItems:"center",
+        flexDirection:'column'
+    },
+
+    image : {
+        width: 60,
+        height: 60,
+        borderRadius:50
+
+
+    }
+    
+
+
+}
--- src/Screens/Tabs/Home.js
+++ src/Screens/Tabs/Home.js
@@ -2,12 +2,16 @@ import { View, Text, ScrollView } from 'react-native'
 import React from 'react'
 import Swiper from '../../Components/Swiper';
 import Favorite from '../../Components/Favorite';
+import Categories from '../../Components/Categories';
 
 const Home = () => {
   return (
     <ScrollView>
     <View>
       <Swiper></Swiper>
+    </View>
+    <View>
+      <Categories></Categories>
     </View>
     <View>
       <Favorite></Favorite>
